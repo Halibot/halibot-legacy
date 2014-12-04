331d6b2cd938ace3eb77e916319dace125e5969c
Chris Harding <hardinc@clarkson.edu>
2014-12-04 18:59:11 -0500

    Ported duckduckgo module

---
+++ modules/ddg.py
@@ -0,0 +1,21 @@
+from module import XMPPModule
+import requests
+
+class Xkcd(XMPPModule):
+	def handleMessage(self, msg):
+		if msg['body'].split(' ')[0] == '!ddg':
+			string = ' '.join(msg['body'].split(' ')[1:])
+
+			if string == '':
+				self.xmpp.reply(msg, "It is kind of hard to search for nothing...")
+				return
+
+			r = requests.get("http://api.duckduckgo.com/?q={}&format=json&no_html=1".format(string))
+			if r.status_code == 200:
+				if r.json()['Abstract'] != '':
+					self.xmpp.reply(msg, r.json()['Abstract'])
+				else:
+					self.xmpp.reply(msg, "No result found :(")
+			else:
+				self.xmpp.reply(msg, "I cannot quack, I can only cry")
+
