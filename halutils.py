2abdb9aa958b505aea2a23e16c973d8197426ac2
Eric Richter <richteer@clarkson.edu>
2014-12-06 15:28:49 -0500

    Added new command splitting utils

--- halutils.py
+++ halutils.py
@@ -4,4 +4,12 @@ def getSensibleName(msg):
 	elif msg["type"] in ("groupchat"):
 		return msg["from"].split("/")[1]
 
+def splitArgStr(msg):
+	body = msg['body'].split(" ", 1)
 
+	return (body[0], body[1]) if len(body) == 2 else (body[0], "")
+
+def splitArgList(msg):
+	body = msg['body'].split(" ")
+
+	return (body[0], body[1:]) if len(body) > 1 else (body[0], [])
