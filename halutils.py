4b9ba7ef09cc6abf1f36086c5a5fad95d14fac34
richteer <richteer@clarkson.edu>
2015-04-20 13:14:08 -0400

    Fixed getSensibleName to now...work

--- halutils.py
+++ halutils.py
@@ -1,8 +1,8 @@
 def getSensibleName(msg):
 	if msg["type"] in ("normal", "chat"):
-		return msg["from"].split("@")[0]
+		return str(msg["from"]).split("@")[0]
 	elif msg["type"] in ("groupchat"):
-		return msg["from"].split("/")[1]
+		return str(msg["from"]).split("/")[1]
 
 def splitArgStr(msg):
 	body = msg['body'].split(" ", 1)
